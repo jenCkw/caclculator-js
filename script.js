76ce9d01e499de8d940b7892f1bd55243e32b8a1
jenCkw <cikwaninejenny@gmail.com>
2022-07-04 12:10:02 +0100

    gestion d'erreur avec try catch

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let buttons = Array.from(document.getElementsByClassName('button')); //Array.fro
 //creation nouveau tableau avec les resultats de la fonction sur chaque element du tableau
 buttons.map(button => {
     button.addEventListener('click', (e) => {
+        console.log(e.target.innerText)
        switch(e.target.innerText){
            case 'c': 
                 display.innerText = '';
@@ -14,10 +15,15 @@ buttons.map(button => {
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
-           case '*':
-                if(display.innerText) {
-                    display.innerText = eval(display.innerText);
-                }
+
+           case '=': 
+               // display.innerText = "*";
+
+               try {
+                display.innerText = eval(display.innerText);
+               } catch {
+                   display.innerText = 'Error';
+               }
                 break;
            default:
                display.innerText += e.target.innerText;
