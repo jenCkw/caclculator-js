01c8ce270c3284f96c17d9be06ab070f0940afd0
jenCkw <cikwaninejenny@gmail.com>
2022-07-04 11:55:41 +0100

    eval qui prend une chaine comme un code java script code

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button')); //Array.from empeche les elements des s'afficher sous forme d'array dans la console.
 
-
+//creation nouveau tableau avec les resultats de la fonction sur chaque element du tableau
 buttons.map(button => {
     button.addEventListener('click', (e) => {
-        console.log('clicked');
-        console.log(e);
-        console.log(e.target);
-        console.log(e.target.innerText);
-    })
-})
+       switch(e.target.innerText){
+           case 'c': 
+                display.innerText = '';
+                break;
+
+           case '←': 
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+           case '*':
+                if(display.innerText) {
+                    display.innerText = eval(display.innerText);
+                }
+                break;
+           default:
+               display.innerText += e.target.innerText;
+       }
+    });
+});
